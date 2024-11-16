96f8c1a2e5ad05ff975f7d8e76c099ddf3b07e86
victor-kiptoo-001 <veelangat680@gmail.com>
2024-11-16 11:56:41 +0300

    Updated URL

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                //href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/victor-kiptoo-001/My-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
